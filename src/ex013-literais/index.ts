b68889eac0a8e9dfce60e7b327da010777732aa0
João Damasio <joaodamasio039@gmail.com>
2026-05-08 14:50:35 -0300

    literais

---
+++ src/ex013-literais/index.ts
@@ -0,0 +1,16 @@
+let x = 10;
+x = 0b1010;
+const y = 10;
+const a = 100;
+const person = {
+    name: 'John',
+    sobrename: 'Doe',
+}
+
+function escolhaCor(cor: 'red' | 'green' | 'blue'):string {
+    return cor;
+}
+
+console.log(escolhaCor('red'));
+
+export default 1;
